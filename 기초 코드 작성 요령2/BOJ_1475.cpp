58a28556e5de1a9e918aa2e7b44ef6642f34c745
songhee1 <ghenrhkwk88@naver.com>
2023-01-16 12:34:09 +0900

    BOJ_1475

--- 기초 코드 작성 요령2/BOJ_1475.cpp
+++ 기초 코드 작성 요령2/BOJ_1475.cpp
@@ -1,3 +1,21 @@
+using namespace std;
+#include<iostream>
+
 int main(){
-    
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   int a[10]={};
+   string s;
+   cin>>s;
+   int setNum=0;
+   for(int i=0;i<s.length();i++){
+    if( a[s[i]-48]==0) {
+        setNum++;
+        a[s[i]-48]=1;
+    }else if(a[s[i]-48]==1){
+        if(s[i]-48==6){
+            
+        }
+    }
+   }
 }
