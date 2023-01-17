cccecb0f64091de7ce7a6a3bada9179fc261364f
songhee1 <ghenrhkwk88@naver.com>
2023-01-17 14:12:10 +0900

    BOJ_1475

--- 기초 코드 작성 요령2/BOJ_1475.cpp
+++ 기초 코드 작성 요령2/BOJ_1475.cpp
@@ -1,21 +1,20 @@
 using namespace std;
-#include<iostream>
-
-int main(){
-   ios::sync_with_stdio(0);
-   cin.tie(0);
-   int a[10]={};
-   string s;
-   cin>>s;
-   int setNum=0;
-   for(int i=0;i<s.length();i++){
-    if( a[s[i]-48]==0) {
-        setNum++;
-        a[s[i]-48]=1;
-    }else if(a[s[i]-48]==1){
-        if(s[i]-48==6){
-            
-        }
+#include <iostream>
+#include <algorithm>
+#include <vector>
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    string N;
+    cin >> N;
+    for (int i = 0; i < N.length(); i++)
+    {
+        v.erase(remove(v.begin(), v.end(), N[i] - 48));
     }
-   }
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i];
+    }
 }
