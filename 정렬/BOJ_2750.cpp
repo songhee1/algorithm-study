43c6565121855679014831829f9261afd352588c
songhee1 <ghenrhkwk88@naver.com>
2023-02-09 00:00:39 +0900

    BOJ_2750

---
+++ 정렬/BOJ_2750.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N, num[1005];
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N;
+    for(int i=0;i<N;i++){
+        cin>>num[i];
+    }
+    int min=num[0];
+    for(int i=i;i<N;i++){
+        if(min>num[i])
+    }
+    
+}
