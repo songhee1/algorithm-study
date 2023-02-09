e3c2c795070098196840e83eae47c89ac0a18913
songhee1 <ghenrhkwk88@naver.com>
2023-02-09 23:50:59 +0900

    BOJ_2750(amend)

--- 정렬/BOJ_2750.cpp
+++ 정렬/BOJ_2750.cpp
@@ -1,16 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int N, num[1005];
-int main(){
+int N;
+int tmp;
+int a[1005];
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin>>N;
-    for(int i=0;i<N;i++){
-        cin>>num[i];
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a[i];
     }
-    int min=num[0];
-    for(int i=i;i<N;i++){
-        if(min>num[i])
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if (a[i] > a[j])
+            {
+                tmp = a[j];
+                a[j] = a[i];
+                a[i] = tmp;
+            }
+        }
     }
-    
+    cout<<"\n";
+    for (int i = 0; i < N; i++)
+    {
+        cout << a[i] << "\n";
+    }
 }
