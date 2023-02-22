053b56f36d39754cb3ed6a840c967ef16fabecfb
songhee1 <ghenrhkwk88@naver.com>
2023-02-22 22:50:11 +0900

    Pass SWEA_1206

--- SWEA/SWEA_1206.cpp
+++ SWEA/SWEA_1206.cpp
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N;
-int testcase = 10;
+int Bigger(int A, int B){
+    if(A>B) return A;
+    else return B;
+}
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int list[1002] = {};
+    int testcase = 1;
     for(int i=1;i<=testcase;i++)
     {
         cin >> N;
         memset(list,0,sizeof(list));
 
-        for(int i=0;i<N;i++){
-            cin>>list[i];
+        for(int j=0;j<N;j++){
+            cin>>list[j];
         }
-
-        int num = 0;
-        int chai = 0;
-        for (int i = 2; i < N-2; i++)
-
+        
+        int sum=0;
+        for (int j = 2; j < N-2; j++)
         {
-            if (list[i - 2] < list[i] && list[i - 1] < list[i] && list[i + 1] < list[i] && list[i + 2] < list[i])
+            if (list[j - 2] < list[j] && list[j - 1] < list[j] && list[j + 1] < list[j] && list[j + 2] < list[j])
             {
-                int chainat = list[i];
-                chainat = min(chainat, list[i] - list[i - 2]);
-                chainat = min(chainat, list[i] - list[i - 1]);
-                chainat = min(chainat, list[i] - list[i + 1]);
-                chainat = min(chainat, list[i] - list[i + 2]);
-                chai += chainat;
+                int BigSideNum=Bigger(list[j-2],Bigger(list[j-1],Bigger(list[j+1], list[j+2])));
+                int n=list[j]-BigSideNum;
+                sum+=n;
+                
             }
         }
-        cout << "#"<<i<<" "<<chai;
+        cout << "#"<<i<<" "<<sum;
     }
     return 0;
 }
