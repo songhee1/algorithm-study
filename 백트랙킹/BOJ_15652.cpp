16f80777ee00c8ca972e3d45d0202e4b1850cc3e
songhee1 <ghenrhkwk88@naver.com>
2023-02-26 21:39:20 +0900

    BOJ_15650/1/2

---
+++ 백트랙킹/BOJ_15652.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, M;
+int answer[15];
+int isused[15];
+void func(int k)
+{
+    if (k == M)
+    {
+        for (int i = 0; i < M; i++)
+        {
+            cout << answer[i] << ' ';
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (k == 0 || (k != 0 && answer[k - 1] <= i))
+        {
+            answer[k] = i;
+            func(k + 1);
+            answer[k] = 0;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    func(0);
+}
