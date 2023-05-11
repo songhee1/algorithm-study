206d7a8cd083cfa6ced5fcac5407026e8fa6cf6f
songhee1 <ghenrhkwk88@naver.com>
2023-05-12 00:19:31 +0900

    Redo BOJ_1912

--- 동적 프로그래밍/BOJ_1912.java
+++ 동적 프로그래밍/BOJ_1912.java
@@ -5,26 +5,45 @@ public class BOJ_1912 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-    static long p[];
-    static long n, maxnum, sum;
+    static long arr[];
+    static long dir[];
+    static long n, maxnum;
 
     public static void main(String[] args) throws Exception {
         n = Integer.parseInt(br.readLine());
 
-        p = new long[1000000];
+        dir = new long[1000000];
         st = new StringTokenizer(br.readLine());
+        arr = new long[1000000];
 
         for (int i = 0; i < n; i++) {
-            p[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        maxnum = p[0];
-        for(int i=0;i<n;i++){
-            for(int j=i;j<=n;j++){
-                sum+= p[j];
-                maxnum = Math.max(sum, maxnum);
+        /*
+         * 현재 값arr[i]+ dir[i-1], arr[i] 값 비교(더 큰 값을 dir[i]에 대입)
+         * - 단, 둘의 합이 음수일 경우 arr[i]값을 dir[i]에 대입
+         */
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0){
+                dir[0] = arr[0];
+                continue;
             }
+            long r = arr[i] + dir[i - 1];
+            if (r < 0) {
+                dir[i] = arr[i];
+            } else {
+                dir[i] = Math.max(arr[i] + dir[i - 1], arr[i]);
+            }
+
         }
+
+        maxnum = dir[0];
+
+       for(int i=1;i<n;i++){
+            maxnum = Math.max(maxnum, dir[i]);
+       }
 
         System.out.print(maxnum);
         br.close();
