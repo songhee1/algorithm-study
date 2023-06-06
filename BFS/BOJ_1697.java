e31461625de921f756b5b9fe2c45b373e0a4e0ed
songhee1 <ghenrhkwk88@naver.com>
2023-06-06 22:34:34 +0900

    알고리즘

--- BFS/BOJ_1697.java
+++ BFS/BOJ_1697.java
@@ -8,40 +8,58 @@ public class BOJ_1697 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int n, m;
-    static boolean flag;
+    static int arr[];
     static int count = 0;
+    static Queue<Integer> queue = new LinkedList<>();
 
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        int nNum, mNum;
-        nNum = n;
-        mNum = m;
+        arr = new int[100001];
 
-        if (n < m) {
-            flag = true;
-        } else
-            flag = false;
-
-        while (nNum != mNum) {
-            if (flag) {
-                if (nNum > mNum) {
-                    count += (nNum - mNum)-1;
-                    break;
-                } else
-                    nNum *= 2;
-            } else {
-                if (nNum < mNum) {
-                    count += (mNum - nNum)-1;
-                    break;
-                } else
-                    nNum /= 2;
+        queue.add(n);
+        arr[n] = 0;
+        if (n != m) {
+            if (n > m) {
+                System.out.println(n - m);
+                return;
+            } else if (n < m) {
+                bfs();
             }
-            count++;
+        } else if (n == m) {
+            System.out.println(1);
+            return;
         }
 
-        System.out.println(count);
+        System.out.println(arr[m]);
+    }
+
+    static void bfs() {
+
+        while (!queue.isEmpty()) {
+            int rest = queue.poll();
+            if(rest == m){
+                return;
+            }
+            int backOne = rest - 1;
+            int frontOne = rest + 1;
+            int twiceOne = rest * 2;
+            if (backOne < 0 || frontOne >= 100001 || twiceOne >= 100001)
+                continue;
+            if (arr[backOne] == 0) {
+                arr[backOne] = arr[rest] + 1;
+                queue.add(backOne);
+            }
+            if (arr[frontOne] == 0) {
+                arr[frontOne] = arr[rest] + 1;
+                queue.add(frontOne);
+            }
+            if (arr[twiceOne] == 0) {
+                arr[twiceOne] = arr[rest] + 1;
+                queue.add(twiceOne);
+            }
+        }
     }
 }
---
+++ DFS/BOJ_24480.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_24480 {
+    static int n,m, v;
+    //n은 정점수, m은 간선수, v는 시작정점
+    
+    static StringTokenizer st;
+    static boolean visited[];
+    static ArrayList<Integer>[] list;
+    static int count;
+    static int checked[];
+    static int nums;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        checked = new int[n+1];
+        list = new ArrayList[n+1];
+        
+        visited = new boolean [n+1];
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+
+        for(int i=0;i<n+1;i++){
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+        }
+
+        for(int i=0;i<=n;i++){
+            Collections.sort(list[i], Comparator.reverseOrder());
+            System.out.println(list[i]);
+        }
+        
+        dfs(v);
+
+        for(int i=0;i<n;i++){
+            System.out.println(checked[i]);
+        }
+    }
+    static void dfs(int node){
+        visited[node] = true;
+        checked[node] = count;
+        
+        for(int i=0;i<list[node].size();i++){
+            int nextNode = list[node].get(i);
+            if (visited[nextNode] == false){
+                count++;
+                dfs(nextNode);
+            }
+        }
+    }
+}
