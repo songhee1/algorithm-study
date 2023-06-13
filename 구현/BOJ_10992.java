e7d8587bbb4cc564ae694409899299b27d8dc1df
songhee1 <ghenrhkwk88@naver.com>
2023-06-13 23:49:00 +0900

    Create BOJ_10992.java

---
+++ 구현/BOJ_10992.java
@@ -0,0 +1,15 @@
+package 구현;
+import java.io.*;
+import java.util.*;
+public class BOJ_10992 {
+    static int num;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        num = Integer.parseInt(br.readLine());
+        while(num--> 0){
+            sb.append()
+        }
+    }
+}
