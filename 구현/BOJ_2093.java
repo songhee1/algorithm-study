79cd497fc2e17b13a9a0b46d95427b8da33c62e9
songhee1 <ghenrhkwk88@naver.com>
2023-06-15 23:53:58 +0900

    Create BOJ_2093.java

---
+++ 구현/BOJ_2093.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2093 {
+     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     static int n;
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+
+        
+    }
+}
