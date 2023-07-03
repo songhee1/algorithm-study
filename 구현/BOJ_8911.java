a9219654a19bf26eea930c37471037611f33f864
songhee1 <ghenrhkwk88@naver.com>
2023-07-03 23:52:59 +0900

    Create BOJ_8911.java

---
+++ 구현/BOJ_8911.java
@@ -0,0 +1,14 @@
+package 구현;
+import java.io.*;
+import java.util.*;
+/*
+ * 명령 (F- 한 눈금 앞으로/B-한 눈금 뒤로/왼쪽으로 90도 회전/오른쪽으로 90도 회전)
+ * 거북이가 지나간 영역을 모두 포함할 수 있는 가장 작은 직사각형 넓이를 구하라
+ * 
+ * 거북이가 이동한 영역 계산
+ */
+public class BOJ_8911 {
+    public static void main(String[] args) {
+        
+    }
+}
