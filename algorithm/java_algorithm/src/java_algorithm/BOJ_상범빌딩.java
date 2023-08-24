e3d47568daace4e99855cf06de56e149374fe83e
songhee <96781855+songhee1@users.noreply.github.com>
2023-08-24 15:29:14 +0900

    Update BOJ_상범빌딩.java

--- algorithm/java_algorithm/src/java_algorithm/BOJ_상범빌딩.java
+++ algorithm/java_algorithm/src/java_algorithm/BOJ_상범빌딩.java
@@ -1,11 +1,11 @@
-package java_algorithm;
+package bfs;
 import java.io.*;
 import java.util.*;
-class Pair{
+class Info{
 	int floor;
 	int x;
 	int y;
-	Pair(int floor, int x, int y){
+	Info(int floor, int x, int y){
 		this.floor = floor;
 		this.x = x;
 		this.y = y;
@@ -14,116 +14,138 @@ class Pair{
 public class BOJ_상범빌딩 {
 	public static List<char[][]> list = new LinkedList<>();
 	public static int[][][] dlist;
-	public static Queue<Pair> q = new LinkedList<>();
+	public static Queue<Info> q = new LinkedList<>();
 	public static int L, R, C;
+	public static int minNum = Integer.MAX_VALUE;
 	public static StringBuilder sb = new StringBuilder();
 	public static int dx[] = {1,0,-1,0}, dy[] = {0,1,0,-1};
 	public static void main(String[] args) throws Exception {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		Scanner sc = new Scanner(System.in);
+
 		while(true) {
+			minNum = Integer.MAX_VALUE;
+			q.clear();
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			L = Integer.parseInt(st.nextToken());
 			R = Integer.parseInt(st.nextToken());
 			C = Integer.parseInt(st.nextToken());
-			
-			if(L == 0 && R == 0 && C == 0) {
+
+			if(L == 0) {
 				break;
 			}
 			
-			char [][]di = new char[R][C];
-			for(int i=0;i<R;i++) {
-				for(int j=0;j<C;j++) {
-					di[i][j] = '#';
-				}
-			}
+			dlist = new int[L][R][C];
 			
-			dlist = new int[L+2][R][C];
-			
-			Pair end = null;
-			list.add(di);
-			
-			for(int h = 1;h<=L;h++) {
+			for(int h = 0;h<L;h++) {
 				char [][]arr = new char[R][C];
-				
-				for(int i=0;i<R;i++) {
+
+				for(int i=0;i<R+1;i++) {
 					String s = br.readLine();
+					if(i == R) break;
 					for(int j=0;j<C;j++) {
 						arr[i][j] = s.charAt(j);
 						if(arr[i][j] == 'S') {
-							q.add(new Pair(h, i, j));
+							q.add(new Info(h, i, j));
+						}
+					}
+
+				}
+				list.add(arr);
+
+			}
+
+			find();
+			
+			if(minNum == Integer.MAX_VALUE) {
+				sb.append("Trapped!").append("\n");
+			}else {
+				sb.append("Escaped in "+ minNum
+						+" minute(s).").append("\n");
+			}
+			
+			
+			
+			
+		}
+
+		System.out.println(sb);
+	}
+
+	public static void find() {
+		while(!q.isEmpty()) {
+			Info p = q.poll();
+			
+			if(L != 1) {
+				
+			
+			
+				if(p.floor == 0) {
+						if(dlist[p.floor+1][p.x][p.y] == 0 
+								&& list.get(p.floor+1)[p.x][p.y] != '#') {
+							dlist[p.floor+1][p.x][p.y] = dlist[p.floor][p.x][p.y] + 1;
+							q.add(new Info(p.floor+1, p.x, p.y));
+							
+							if(list.get(p.floor+1)[p.x][p.y] == 'E') {
+								minNum = Math.min(minNum, dlist[p.floor+1][p.x][p.y]);
+								return;
+							}
+						}
+					
+					
+				}else if(p.floor == L-1) {
+					if(dlist[p.floor-1][p.x][p.y] == 0 
+							&& list.get(p.floor-1)[p.x][p.y] != '#') {
+						
+						dlist[p.floor-1][p.x][p.y] = dlist[p.floor][p.x][p.y] + 1;
+						q.add(new Info(p.floor-1, p.x, p.y));
+						
+						if(list.get(p.floor-1)[p.x][p.y] == 'E') {
+							minNum = Math.min(minNum, dlist[p.floor-1][p.x][p.y]);
+							return;
 						}
 					}
 					
-				}
-				String a = br.readLine();
-				list.add(arr);
+				}else {
+				
+			
+					if(dlist[p.floor-1][p.x][p.y] == 0 
+							&& list.get(p.floor-1)[p.x][p.y] != '#') {
+						
+						dlist[p.floor-1][p.x][p.y] = dlist[p.floor][p.x][p.y] + 1;
+						q.add(new Info(p.floor-1, p.x, p.y));
+						
+						if(list.get(p.floor-1)[p.x][p.y] == 'E') {
+							minNum = Math.min(minNum, dlist[p.floor-1][p.x][p.y]);
+							return;
+						}
+					}
 	
-			}
-			
-			char [][]di2 = new char[R][C];
-			for(int i=0;i<R;i++) {
-				for(int j=0;j<C;j++) {
-					di2[i][j] = '#';
+					if(dlist[p.floor+1][p.x][p.y] == 0 
+							&& list.get(p.floor+1)[p.x][p.y] != '#') {
+						dlist[p.floor+1][p.x][p.y] = dlist[p.floor][p.x][p.y] + 1;
+						q.add(new Info(p.floor+1, p.x, p.y));
+						
+						if(list.get(p.floor+1)[p.x][p.y] == 'E') {
+							minNum = Math.min(minNum, dlist[p.floor+1][p.x][p.y]);
+							return;
+						}
+					}
 				}
 			}
 			
-			list.add(di2);
-			
-			find();
-			
-//			for(int i=1;i<=L;i++) {
-//				for(int j=0;j<R;j++) {
-//					for(int k=0;k<C;k++) {
-//						System.out.print(dlist[i][j][k]+" ");
-//					}
-//					System.out.println();
-//				}
-//				System.out.println();
-//			}
-			
-			q.clear();
-			if(dlist[end.floor][end.x][end.y] == 0) {
-				sb.append("Trapped!").append("\n");
-			}else {
-				sb.append("Escaped in "+dlist[end.floor][end.x][end.y]
-						+" minute(s).").append("\n");
-			}
-		}
-		
-		System.out.println(sb);
-	}
-	
-	public static void find() {
-		while(!q.isEmpty()) {
-			Pair p = q.poll();
-			if(dlist[p.floor-1][p.x][p.y] == 0 
-					&& list.get(p.floor-1)[p.x][p.y] != '#') {
-				dlist[p.floor-1][p.x][p.y] = dlist[p.floor][p.x][p.y] + 1;
-				q.add(new Pair(p.floor-1, p.x, p.y));
-			}
-			
-			if(dlist[p.floor+1][p.x][p.y] == 0 
-					&& list.get(p.floor+1)[p.x][p.y] != '#') {
-				dlist[p.floor+1][p.x][p.y] = dlist[p.floor][p.x][p.y] + 1;
-				q.add(new Pair(p.floor+1, p.x, p.y));
-			}
-			
+
 			for(int dir = 0;dir<4;dir++) {
 				int nx = p.x + dx[dir];
 				int ny = p.y + dy[dir];
-				
-				if(nx<0 || nx>=R || ny<0 || ny>=C) {
-					continue;
-				}
-				
+
+				if(nx<0 || ny<0 || nx>=R || ny>=C) continue;
 				if(dlist[p.floor][nx][ny] == 0 && list.get(p.floor)[nx][ny] != '#') {
 					dlist[p.floor][nx][ny] = dlist[p.floor][p.x][p.y] + 1;
-					q.add(new Pair(p.floor, nx, ny));
-					
+					q.add(new Info(p.floor, nx, ny));
+
 					if(list.get(p.floor)[nx][ny] == 'E') {
+						minNum = Math.min(minNum, dlist[p.floor][nx][ny]);
 						return;
 					}
 				}
