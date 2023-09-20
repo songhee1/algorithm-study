24c2f2dd1ae7794463eaa7aad08b81681f5f8d45
songhee <96781855+songhee1@users.noreply.github.com>
2023-09-20 09:22:34 +0900

    Update BOJ_2110.java

--- java01_그리디/src/com/edu/test/BOJ_2110.java
+++ java01_그리디/src/com/edu/test/BOJ_2110.java
@@ -1,47 +1,47 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
+package com.edu.test;
+import java.io.*;
+import java.util.*;
 public class BOJ_2110 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+	public static int arr[], maxDist = 0, n, c;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n+1];
+		
+		for(int i=1;i<=n;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		int min_distance = 1;
+		int max_distance = arr[n]-arr[1];
+		
+		while(min_distance<=max_distance) {
+			int mid_distance = (min_distance+max_distance)/2;
+			int standard = arr[1]; //비교거리 기준이 되는 집
+			int count = 1; //공유기 설치 대수. 처음에는 첫번째 arr[1]위치인 집에 공유기를 설치한다고 가정하고 시작.
+			for(int i=1;i<n+1;i++){
+				if(arr[i]-standard>=mid_distance) {
+					count++;
+					standard = arr[i];
+				}
+			}
+			
+			if(count < c) {
+				max_distance = mid_distance -1;
+			}else if(count >= c) {
+				maxDist = Math.max(maxDist, mid_distance);
+				System.out.println(mid_distance);
+				min_distance = mid_distance +1;
+			}
+		}
+		
+		System.out.println(maxDist);
+	}
 
-        int N = input.nextInt();  // 집의 개수
-        int C = input.nextInt();  // 공유기의 개수
-        int[] homeList = new int[N + 1];
-
-        for (int i = 1; i <= N; ++i) {
-            homeList[i] = input.nextInt();
-        }
-
-        Arrays.sort(homeList);   // 정렬
-
-        int maxResult = 0;
-
-        int left = 1;
-        int right = homeList[N] - homeList[1];
-        int d = 0;
-        int ans = 0;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int start = homeList[1];
-            int count = 1;  // 공유기 설치 GAP 저장
-            for (int i = 1; i <= N; ++i) {
-                d = homeList[i] - start;  // 집마다 거리 체크
-                if (d >= mid) {  // 공유기 설치 가능한지 여부 체크
-                    count++;
-                    start = homeList[i]; // 설치 했다면 여기 집 부터 다시 거리 체크
-                }
-            }
-
-            if (count >= C) {
-                ans = mid;
-                left = mid + 1;  // 더 많은 Gap에서 공유기 설치할 수 있는지 여부 확인
-            } else {
-                right = mid - 1; // 더 적은 Gap에서 공유기 설치할 수 있는지 여부 확인
-            }
-        }
-
-        System.out.println(ans);
-    }
 }
