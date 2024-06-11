7fc153209bb9d2b6fa5056663d8e5b08757ca2fc
songhee <96781855+songhee1@users.noreply.github.com>
2024-06-11 22:43:53 +0900

    Create BOJ_단거리.java

---
+++ 그리디/BOJ_단거리.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    static class Road{
+        int start;
+        int end;
+        int dist;
+        Shortcut(int start, int end, int dist){
+            this.start = start;
+            this.end = end;
+            this.dist = dist;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int D = sc.nextInt();
+        ArrayList<Shortcut> path = new ArrayList<>();
+
+        for(int i = 0; i < N; i++){
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            int dist = sc.nextInt();
+            if (end > D)
+                continue;
+            if (end - start > dist)
+                path.add(new Shortcut(start, end, dist));
+        }
+
+        Collections.sort(path, new Comparator<Shortcut>() {
+            public int compare(Shortcut o1, Shortcut o2) {
+                if (o1.start == o2.start){
+                    return Integer.compare(o1.end, o2.end);
+                }  
+                return Integer.compare(o1.start, o2.start);
+            }
+        });
+
+        int[] d = new int[D+1];
+        int move = 0;
+        int idx = 0;
+        Arrays.fill(d, Integer.MAX_VALUE);
+        d[0] = 0;
+        while (move < D){
+            if (idx < path.size()){
+                Road s = path.get(idx);
+                if (move == s.start){
+                    d[s.end] = Math.min(d[move] + s.dist, d[s.end]);
+                    idx++;
+                } else {
+                  d[move + 1] = Math.min(d[move + 1], d[move] + 1);
+                  move++;
+                }
+            } else {
+                d[move + 1] = Math.min(d[move + 1], d[move] + 1);
+                move++;
+            }
+        }
+        System.out.println(d[D]);
+
+    }
+}
